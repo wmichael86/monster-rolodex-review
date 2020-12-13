0626b0c7f3e965df5d16b35c5143541a5586bc2d
Michael Lewis <wmichael.lewis@outlook.com>
2020-12-12 21:41:47 -0800

    resorted back to published version of app

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import React, {Component} from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import {SearchBox} from './components/search-box/search-box.component';
 import './App.css';
-import { MONSTERS } from '../src/shared/monsters'
 
 class App extends Component {
   constructor() {
     super();
       this.state = {
-        monsters: MONSTERS,
+        monsters: [],
         searchField: ''
       }
   }
 
-  // componentDidMount() {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //   .then(response => 
-  //     response.json())
-  //     .then(users => this.setState({ monsters: users }));
-  // }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => 
+      response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
 
 
   render() {
